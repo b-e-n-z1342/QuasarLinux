4bcd0b36a047335b7b919ec45ab2f4dda8f4a781
QuasarTeams <kzn45@list.ru>
2025-07-24 00:16:21 +0300

    Create INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+read -p "у вас WIFI ?  (y/N): " iwifi
+if [[ ! "$iwifi" =~ ^[Yy]$ ]]; then 
+	echo "ok"
+	./INST.sh
+	exit 0
+fi
+
+
+echo "Давайте сперва подключимся к интернету через iwd"
+echo "Это не страшно! "
+
+echo "сейчас вы видите какие у вас сетевые карты"
+
+echo "выберите какую вы будете использовать (ничего вводить не надо)"
+echo "после выбора напишите"
+
+echo "station <интерфейс> scan"
+echo "после этого" 
+
+echo "station <интерфейс> get-networks "
+
+echo "далее"
+
+
+echo "station <> connect <название вашей сети wifi>"
+echo "после этого ведёте пароль и закройте её командой: exit"
+iwctl device list 
+iwctl
+
+./INST.sh
