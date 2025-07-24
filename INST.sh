6b98a95b762eda736f83e991fced96e8ca5ad0c7
QuasarTeams <kzn45@list.ru>
2025-07-24 13:55:49 +0300

    Update INST.sh

--- INST.sh
+++ INST.sh
@@ -21,7 +21,7 @@ echo "установка Plasma"
 sudo pacman -Syy
 
 
-sudo pacman -S --noconfirm plasma seatd go sddm sddm-openrc dolphin qt6 wine-staging winetricks kcalc gwenview kate vlc konsole mesa vulkan-tools gamemode lib32-gamemode lib32-alsa-plugins  lib32-libpulse pipewire gst-plugins-base gst-plugins-good  gst-plugins-bad  gst-plugins-ugly xorg xorg-xinit pavucontrol networkmanager networkmanager-openrc sddm sddm-openrc
+sudo pacman -S --noconfirm plasma seatd go sddm sddm-openrc dolphin qt6 wine-staging winetricks kcalc gwenview kate vlc konsole mesa vulkan-tools gamemode lib32-gamemode lib32-alsa-plugins  lib32-libpulse pipewire gst-plugins-base gst-plugins-good  gst-plugins-bad  gst-plugins-ugly xorg xorg-xinit pavucontrol networkmanager networkmanager-openrc sddm sddm-openrc flapak 
 wineboot --init
 winetricks dxvk vkd3d corefonts vcrun2012 vcrun2013 vcrun2015 vcrun2019 vcrun2022 dotnet40 dotnet45 dotnet462 dotnet48 win10 --force -q --unattended
 #WINEDLLOVERRIDES="mscoree,mshtml=" WINEARCH=win64 winetricks --unattended --force -q corefonts tahoma cjkfonts vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 vcrun2019 vcrun2022  dotnet40 dotnet45 dotnet462 dotnet48 dotnetcoredesktop3 dotnetcoredesktop6 d3dcompiler_43 d3dcompiler_47 d3dx9 d3dx10 d3dx11_43 directx9 directx10 directx11 xact xinput quartz devenum wmp9 wmp10 wmp11 msxml3 msxml4 msxml6 gdiplus riched20 riched30 vb6run mfc40 mfc42 mfc70 mfc80 mfc90 mfc100 mfc110 mfc140 ie8 flash silverlight physx openal dsound xna40 faudio dxvk vkd3d dgvoodoo2 win10
@@ -43,6 +43,8 @@ sudo mkdir -p /var/lib/sddm /var/run/sddm
 sudo chown sddm:sddm /var/lib/sddm /var/run/sddm
 sudo chmod 0755 /var/lib/sddm /var/run/sddm
 sudo usermod -aG seat,video,input sddm
+
+
 
 # Создание OpenRC скрипта для SDDM
 sudo tee /etc/init.d/sddm << 'SDDM_EOF'
@@ -91,8 +93,8 @@ sudo chmod +x /etc/init.d/sddm
 sudo mkdir -p /etc/sddm.conf.d
 sudo tee /etc/sddm.conf.d/quasar.conf << 'SDDM_CONF_EOF'
 [General]
-HaltCommand=/usr/bin/systemctl poweroff
-RebootCommand=/usr/bin/systemctl reboot
+HaltCommand=/usr/bin/poweroff
+RebootCommand=/usr/bin/reboot
 
 [X11]
 SessionDir=/usr/share/xsessions
@@ -112,7 +114,7 @@ sudo pacman -Rn jack2
 sudo pacman -S --noconfirm pipewire lib32-libpipewire libpipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber pipewire-audio pipewire-openrc pipewire-pulse-openrc lib32-pipewire-jack
 
 # Создание OpenRC скрипта для pipewire
-sudo tee /etc/init.d/pipewire << 'PIPEWIRE_EOF'
+sudo tee /etc/init.d/pipewire << 'EOF'
 #!/sbin/openrc-run
 command="/usr/bin/pipewire"
 command_user="root"
@@ -141,8 +143,28 @@ echo "Версия: $(cat /etc/os-release | grep VERSION= | cut -d'=' -f2 | tr -
 echo ""
 BASHRC_EOF
 
+sudo rc-update add saetd default
+sudo rc-update add elogind defailt
+sudo rc-update add pipewire-pulse default
 
 
+
+cat /etc/local.d/fixing.start << 'EOF'
+#!/bin/bash
+if [ -z "DBUS_SESSION_BUS_ADDRESS" ]; then
+    eval "$(dbus-launch --sh-syntax)"
+fi
+
+export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+[ -d "$XDG_RUNTIME_DIR" ] || {
+    mkdir -p "$XDG_RUNTIME_DIR"
+    chmod 700 "$XDG_RUNTIME_DIR"
+}
+EOF
+
+sudo chmod +x /etc/local.d/fixing.start
+
+sudo rc-update add local default
 
 
 
