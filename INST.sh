71c32205df3ca99eb8e2a35e11f5dac9bced855d
QuasarTeams <kzn45@list.ru>
2025-07-30 18:27:57 +0300

    Update INST.sh

--- INST.sh
+++ INST.sh
@@ -48,12 +48,14 @@ winetricks dxvk vkd3d corefonts vcrun2012 vcrun2013 vcrun2015 vcrun2019 vcrun202
 #WINEDLLOVERRIDES="mscoree,mshtml=" WINEARCH=win64 winetricks --unattended --force -q mono gecko corefonts tahoma cjkfonts vcrun6 vcrun2003 vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 vcrun2019 vcrun2022 dotnet20 dotnet30 dotnet35 dotnet40 dotnet45 dotnet462 dotnet48 dotnetcoredesktop3 dotnetcoredesktop6 d3dcompiler_43 d3dcompiler_47 d3dx9 d3dx10 d3dx11_43 directx9 directx10 directx11 xact xinput quartz devenum wmp9 wmp10 wmp11 msxml3 msxml4 msxml6 gdiplus riched20 riched30 vb6run mfc40 mfc42 mfc70 mfc80 mfc90 mfc100 mfc110 mfc140 ie8 flash silverlight physx openal dsound xna40 faudio dxvk vkd3d dgvoodoo2 win10 >/dev/null 2>&1
 
 winetricks win10
+sleep 3
+
 git clone https://aur.archlinux.org/yay-bin
 cd yay-bin
 makepkg -si --noconfirm
 
 yay -S waydroid --noconfirm
-
+waydroid -init
 
 echo "Настройка SDDM..."
 sudo groupadd -f sddm
