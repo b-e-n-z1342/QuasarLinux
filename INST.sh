87b8639cde8050e76f9c32637c3440855cf763b2
QuasarTeams <kzn45@list.ru>
2025-08-06 11:53:44 +0300

    Update INST.sh

--- INST.sh
+++ INST.sh
@@ -1,5 +1,5 @@
 echo "=========================================="
-echo "    QUASAR LINUX ALPHA - ПОСТ-УСТАНОВКА"
+echo "    QUASAR LINUX BETA - ПОСТ-УСТАНОВКА"
 echo "=========================================="
 echo ""
 echo "Добро пожаловать в Quasar Linux!"
@@ -22,7 +22,7 @@ sudo pacman -Syy
 
 
 sudo pacman -S --noconfirm plasma seatd go sddm sddm-openrc dolphin qt6 wine-staging winetricks qt6-tools qt5-tools kcalc gwenview kate vlc konsole mesa vulkan-tools gamemode lib32-gamemode lib32-alsa-plugins  lib32-libpulse pipewire gst-plugins-base gst-plugins-good  gst-plugins-bad  gst-plugins-ugly pavucontrol flatpak 
-sleep 5
+sleep 2
 
 sudo rc-update add seatd default
 
@@ -38,17 +38,17 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 
 clear
-sleep 5
+sleep 1
 echo "Настройка Wine"
 wineboot --init
 
 sleep 2
 
-
+clear
 
 winetricks --force -q --unattended corefonts tahoma cjkfonts vcrun6 vcrun2003 vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 vcrun2019 vcrun2022 dotnet20 dotnet30 dotnet35 dotnet40 dotnet45 dotnet462 dotnet48 dotnetcoredesktop3 dotnetcoredesktop6 d3dcompiler_43 d3dcompiler_47 d3dx9 d3dx10 d3dx11_43 directx9 directx10 directx11 xact xinput quartz devenum wmp9 wmp10 wmp11 msxml3 msxml4 msxml6 gdiplus riched20 riched30 vb6run mfc40 mfc42 mfc70 mfc80 mfc90 mfc100 mfc110 mfc140 ie8 flash silverlight physx openal dsound xna40 faudio dxvk vkd3d dgvoodoo2 win10
 
-sleep 3
+sleep 2
 clear
 
 git clone https://aur.archlinux.org/yay-bin
@@ -139,31 +139,13 @@ sudo pacman -S --noconfirm polkit polkit-qt6 polkit-kde-agent
 
 
 
-elogind_conf="/etc/elogind/logind.conf"
-add_or_replace_conf_param() {
-    local key="$1"
-    local value="$2"
-    if grep -q "^$key=" "$elogind_conf"; then
-        sudo sed -i "s|^$key=.*|$key=$value|" "$elogind_conf"
-    else
-        echo "$key=$value" | sudo tee -a "$elogind_conf" > /dev/null
-    fi
-}
-
-add_or_replace_conf_param "HandlePowerKey" "poweroff"
-add_or_replace_conf_param "HandleSuspendKey" "suspend"
-add_or_replace_conf_param "HandleHibernateKey" "hibernate"
-add_or_replace_conf_param "HandleLidSwitch" "suspend"
-
-
-
 sudo chmod +x /etc/local.d/fixing.start
 sudo rc-update add local default
 sudo pacman -Scc --noconfirm
 clear
 
-echo "Установка завершена! Перезагрузка через 10 секунд..."
-sleep 10
+echo "Установка завершена! Перезагрузка через 5 секунд..."
+sleep 5
 reboot
 
 
